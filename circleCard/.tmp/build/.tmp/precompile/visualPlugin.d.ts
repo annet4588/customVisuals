97624c2128fcd3e144e19a01f01d273470f6a15a
annet4588 <annet4588@yahoo.co.uk>
2025-06-01 13:02:58 +0100

    Set environment, build a KPI Box Visual

---
+++ circleCard/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var circleCard785E34FA3E4D4BC2885FD36CA249C315_DEBUG: IVisualPlugin;
+export default circleCard785E34FA3E4D4BC2885FD36CA249C315_DEBUG;
